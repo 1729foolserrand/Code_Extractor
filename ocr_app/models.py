48316f951cb492431b7dc413c833db61e599db27
1729foolserrand <noonsavathgn@gmail.com>
2025-08-06 19:29:33 +0530

    Add media file handling

---
+++ ocr_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to="uploads/")
--- ocr_app/urls.py
+++ ocr_app/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 
 from . import views
@@ -5,3 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.upload_image, name="upload_image"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
