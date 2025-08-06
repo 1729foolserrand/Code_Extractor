cb1cb5758c2dcbfee772fec645fe73e3bb43a346
1729foolserrand <noonsavathgn@gmail.com>
2025-08-06 16:18:32 +0530

    modified settings.py

--- ocr_webapp/settings.py
+++ ocr_webapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["your-domain.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
