bea164e0fd4600323f73a5128ee2701de31355e6
1729foolserrand <noonsavathgn@gmail.com>
2025-08-06 18:55:32 +0530

    modified settings

--- ocr_webapp/settings.py
+++ ocr_webapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["code-extractor-5.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
